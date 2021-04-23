1a0a16f1ec05dd63217d43c1e55f9e8d135cfec9
thinkphp <mergesortv@gmail.com>
2021-04-23 16:38:28 +0300

    second commit

--- app.go
+++ app.go
@@ -3,12 +3,22 @@ package main
 import (
   "fmt"
   "net/http"  
+  "os"
 )
+
+func getPort() string {
+
+	p := os.Getenv("PORT")
+	if p != "" {
+		return ":" + p
+	}
+	return ":8080"
+}
 
 func main() {
 
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":5000", nil)
+    http.ListenAndServe(getPort(), nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
